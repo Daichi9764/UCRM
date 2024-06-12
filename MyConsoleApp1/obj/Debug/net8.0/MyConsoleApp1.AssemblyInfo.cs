efa233a141c34051a00b6d68910531d517286d8e
MaximeO <maxime.oliveira40@gmail.com>
2024-06-12 15:27:24 +0200

    Déplacement de la gestion des fichiers dans sa propre classe séparée

--- MyConsoleApp1/obj/Debug/net8.0/MyConsoleApp1.AssemblyInfo.cs
+++ MyConsoleApp1/obj/Debug/net8.0/MyConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+346144c217a73e5e9bb40c16aec97f6cf21b3a2b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5037f6e9209a219fc8e86ca81d3335560ef6db8")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ MyConsoleApp1/projectFileManager.cs
@@ -0,0 +1,98 @@
+﻿namespace MyConsoleApp1;
+
+public class projectFileManager
+{
+    // ----- Attributs privés -----
+    private string knxprojSourcePath; // Adresse du fichier du projet
+    private string knxprojExportPath; // Adresse du dossier projet exporté
+    private string zipArchivePath; // Adresse du fichier zip (utile pour la suite de manière à rendre le projet extractable)
+    
+    // ----- Attributs publics -----
+    
+    // ----- Méthodes privées -----
+    
+    // ----- Méthodes publiques -----
+    public projectFileManager()
+    {
+        knxprojSourcePath = "";
+        knxprojExportPath = "";
+        zipArchivePath = "";
+    }
+
+    
+    
+    public projectFileManager(string source, string export)
+    {
+        knxprojSourcePath = source;
+        knxprojExportPath = export;
+        zipArchivePath = "";
+    }
+
+    
+    
+    public void extractProjectFiles()
+    {
+        /* ------------------------------------------------------------------------------------------------
+        ---------------------------------------- GESTION DES PATH -----------------------------------------
+        ------------------------------------------------------------------------------------------------ */
+
+        // CETTE PORTION DE CODE PERMET EVENTUELLEMENT DE DEMANDER A L'UTILISATEUR DE DONNER LE PATH DU FICHIER .knxproj
+        // IDEALEMENT DANS LA VERSION "INTERFACE" DU LOGICIEL, ON AURA UN POPUP POUR QUE L'UTILISATEUR SELECTIONNE LE FICHIER
+        // DIRECTEMENT DANS L'ARBORESCENCE WINDOWS.
+        /*string knxprojSourcePath;
+        Console.WriteLine("Veuillez entrer l'adresse du fichier du projet dans l'arborescence des fichiers:");
+        knxprojSourcePath = Console.ReadLine(); // Lecture du path entré par l'utilisateur dans la console */
+
+        knxprojSourcePath = Path.GetFullPath(knxprojSourcePath); // Normalisation de l'adresse du fichier du projet
+        knxprojExportPath = Path.GetFullPath(knxprojExportPath); // Normalisation de l'adresse du dossier projet exporté
+
+
+        /* ------------------------------------------------------------------------------------------------
+        ---------------------------------- EXTRACTION DU FICHIER KNXPROJ ----------------------------------
+        ------------------------------------------------------------------------------------------------ */
+
+        // Transformation du knxproj en zip
+        if (knxprojSourcePath.EndsWith(".knxproj"))
+        {
+            // Si le fichier entré est un .knxproj
+            zipArchivePath =
+                knxprojSourcePath.Substring(0, knxprojSourcePath.Length - ".knxproj".Length) +
+                ".zip"; // On enlève .knxproj et on ajoute .zip
+        }
+        else
+        {
+            // Sinon, ce n'est pas le type de fichier que l'on veut
+            Console.WriteLine("Erreur: le fichier entré n'est pas au format .knxproj. "
+                              + "Veuillez réessayer. Pour obtenir un fichier dont l'extension est .knxproj, "
+                              + "rendez-vous dans votre tableau de bord ETS et cliquez sur \"Exporter le projet\"");
+            Environment.Exit(1); // Arrêt du programme avec un code erreur
+        }
+
+        try
+        {
+            // On essaie de transformer le fichier .knxproj en archive .zip
+            System.IO.File.Move(knxprojSourcePath, zipArchivePath);
+
+        }
+        catch (System.IO.FileNotFoundException)
+        {
+            // Si le fichier n'existe pas ou que le path est incorrect
+            Console.WriteLine("Fichier introuvable. Veuillez vérifier le path que vous avez entré et réessayer.");
+            Environment.Exit(
+                2); // On arrête ici l'application avec un code erreur, mais on pourraît éventuellement redemander un nouveau path
+
+        }
+        catch (UnauthorizedAccessException)
+        {
+            // Si le fichier n'est pas accessible en écriture
+            Console.WriteLine($"Impossible d'accéder en écriture au fichier {knxprojSourcePath}. "
+                              + "Veuillez vérifier que le programme a bien accès au fichier ou tentez de l'exécuter "
+                              + "en tant qu'administrateur.");
+            Environment.Exit(3);
+        }
+
+        // Si le fichier a bien été transformé en zip
+        System.IO.Compression.ZipFile.ExtractToDirectory(zipArchivePath, knxprojExportPath); // On extrait le zip
+        System.IO.File.Delete(zipArchivePath); // On n'a plus besoin du zip, on le supprime
+    }
+}
