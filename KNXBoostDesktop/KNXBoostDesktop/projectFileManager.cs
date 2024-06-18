b4967c4b34a8b609d549fc2383b313e300d1a473
MaximeO <maxime.oliveira40@gmail.com>
2024-06-18 11:55:22 +0200

    Ajouté le logging dans le projectFileManager

--- KNXBoostDesktop/KNXBoostDesktop/projectFileManager.cs
+++ KNXBoostDesktop/KNXBoostDesktop/projectFileManager.cs
@@ -43,175 +43,161 @@ namespace KNXBoostDesktop
             bool managedToExtractProject = false;
             bool managedToNormalizePaths = false;
             bool cancelOperation = false;
-            string msg;
-
-            using (StreamWriter writer = new StreamWriter("test.txt"))
+            
+            // Tant que l'on n'a pas réussi à extraire le projet ou que l'on n'a pas demandé l'annulation de l'extraction
+            while ((!managedToExtractProject) && (!cancelOperation))
             {
-                // Tant que l'on n'a pas réussi à extraire le projet ou que l'on n'a pas demandé l'annulation de l'extraction
-                while ((!managedToExtractProject) && (!cancelOperation))
-                {
-                    /* ------------------------------------------------------------------------------------------------
-                    ---------------------------------------- GESTION DES PATH -----------------------------------------
-                    ------------------------------------------------------------------------------------------------ */
+                /* ------------------------------------------------------------------------------------------------
+                ---------------------------------------- GESTION DES PATH -----------------------------------------
+                ------------------------------------------------------------------------------------------------ */
 
-                    // Répéter tant que l'on n'a pas réussi à normaliser les chemins d'accès ou que l'on n'a pas demandé
-                    // à annuler l'extraction
-                    while ((!managedToNormalizePaths) && (!cancelOperation))
-                    {
-                        if ((KnxprojExportFolderPath.ToLower() == "null") || (KnxprojSourceFilePath.ToLower() == "null"))
-                        {
-                            cancelOperation = true;
-                            msg = $"[{DateTime.Now:dd/MM/yyyy - HH:mm:ss}] Annulation de l'exportation du projet.";
-                            Console.WriteLine(msg);
-                            writer.WriteLine(msg);
-                            continue;
-                        }
-
-                        // On tente d'abord de normaliser l'adresse du fichier du projet
-                        try
-                        {
-                            KnxprojSourceFilePath =
-                                Path.GetFullPath(KnxprojSourceFilePath); // Normalisation de l'adresse du fichier du projet
-                        }
-                        catch (ArgumentException)
-                        {
-                            msg = $"[{DateTime.Now:dd/MM/yyyy - HH:mm:ss}] Erreur: le chemin de source du fichier .knxproj est vide. Veuillez réessayer.";
-                            Console.WriteLine(msg);
-                            writer.WriteLine(msg);
-                            KnxprojSourceFilePath = AskForPath();
-                            continue;
-                        }
-                        catch (PathTooLongException)
-                        {
-                            msg = $"[{DateTime.Now:dd/MM/yyyy - HH:mm:ss}] Erreur: le chemin {KnxprojSourceFilePath} est trop long (plus de 255 caractères). Veuillez réessayer.";
-                            Console.WriteLine(msg);
-                            writer.WriteLine(msg);
-                            KnxprojSourceFilePath = AskForPath();
-                            continue;
-                        }
-
-                        // Une fois que la normalisation de l'adresse du fichier du projet a été effectuée,
-                        // On tente de normaliser l'adresse du dossier projet exporté
-                        try
-                        {
-                            KnxprojExportFolderPath =
-                                Path.GetFullPath(KnxprojExportFolderPath); // Normalisation de l'adresse du dossier projet exporté
-                        }
-                        catch (ArgumentException)
-                        {
-                            msg = $"[{DateTime.Now:dd/MM/yyyy - HH:mm:ss}] Erreur: le chemin d'exportation du projet indiqué est vide. Veuillez réessayer.";
-                            Console.WriteLine(msg);
-                            writer.WriteLine(msg);
-                            KnxprojExportFolderPath = AskForPath();
-                            continue;
-                        }
-                        catch (PathTooLongException)
-                        {
-                            msg = $"[{DateTime.Now:dd/MM/yyyy - HH:mm:ss}] Erreur: le chemin {KnxprojExportFolderPath} est trop long (plus de 255 caractères). Veuillez réessayer.";
-                            Console.WriteLine(msg);
-                            writer.WriteLine(msg);
-                            KnxprojExportFolderPath = AskForPath();
-                            continue;
-                        }
-
-                        managedToNormalizePaths = true;
-                    }
-
-                    /* ------------------------------------------------------------------------------------------------
-                    ---------------------------------- EXTRACTION DU FICHIER KNXPROJ ----------------------------------
-                    ------------------------------------------------------------------------------------------------ */
+                // Répéter tant que l'on n'a pas réussi à normaliser les chemins d'accès ou que l'on n'a pas demandé
+                // à annuler l'extraction
+                string msg;
                     
-                    msg = $"[{DateTime.Now:dd/MM/yyyy - HH:mm:ss}] Starting to extract {Path.GetFileName(KnxprojSourceFilePath)}...";
-                    Console.WriteLine(msg);
-                    writer.WriteLine(msg);
-
-                    string zipArchivePath; // Adresse du fichier zip (utile pour la suite de manière à rendre le projet extractable)
-
-                    // Transformation du knxproj en zip
-                    if (KnxprojSourceFilePath.EndsWith(".knxproj"))
+                while ((!managedToNormalizePaths) && (!cancelOperation))
+                {
+                    if ((KnxprojExportFolderPath.ToLower() == "null") || (KnxprojSourceFilePath.ToLower() == "null"))
                     {
-                        // Si le fichier entré est un .knxproj
-                        zipArchivePath =
-                            KnxprojSourceFilePath.Substring(0, KnxprojSourceFilePath.Length - ".knxproj".Length) +
-                            ".zip"; // On enlève .knxproj et on ajoute .zip
-                    }
-                    else
-                    {
-                        // Sinon, ce n'est pas le type de fichier que l'on veut
-                        msg = $"[{DateTime.Now:dd/MM/yyyy - HH:mm:ss}] Erreur: le fichier entré n'est pas au format .knxproj. "
-                              + "Veuillez réessayer. Pour obtenir un fichier dont l'extension est .knxproj, "
-                              + "rendez-vous dans votre tableau de bord ETS et cliquez sur \"Exporter le projet\"\n";
-                        Console.WriteLine(msg);
-                        writer.WriteLine(msg);
-                        KnxprojSourceFilePath = AskForPath();
-                        continue; // Retour au début de la boucle pour retenter l'extraction avec le nouveau path
+                        cancelOperation = true;
+                        msg = $"[{DateTime.Now:dd/MM/yyyy - HH:mm:ss}] Annulation de l'exportation du projet.";
+                        App.ConsoleAndLogWriteLine(msg);
+                        continue;
                     }
 
+                    // On tente d'abord de normaliser l'adresse du fichier du projet
                     try
                     {
-                        // On essaie de transformer le fichier .knxproj en archive .zip
-                        File.Move(KnxprojSourceFilePath, zipArchivePath);
+                        KnxprojSourceFilePath =
+                            Path.GetFullPath(KnxprojSourceFilePath); // Normalisation de l'adresse du fichier du projet
                     }
-                    catch (FileNotFoundException)
+                    catch (ArgumentException)
                     {
-                        // Si le fichier n'existe pas ou que le path est incorrect
-                        msg = $"[{DateTime.Now:dd/MM/yyyy - HH:mm:ss}] Fichier {KnxprojSourceFilePath} introuvable. Veuillez vérifier le path que vous avez entré et réessayer.\n";
-                        Console.WriteLine(msg);
-                        writer.WriteLine(msg);
+                        msg = $"[{DateTime.Now:dd/MM/yyyy - HH:mm:ss}] Erreur: le chemin de source du fichier .knxproj est vide. Veuillez réessayer.";
+                        App.ConsoleAndLogWriteLine(msg);
                         KnxprojSourceFilePath = AskForPath();
-                        continue; // Retour au début de la boucle pour retenter l'extraction avec le nouveau path
-                    }
-                    catch (UnauthorizedAccessException)
-                    {
-                        // Si le fichier n'est pas accessible en écriture
-                        msg = $"[{DateTime.Now:dd/MM/yyyy - HH:mm:ss}] Impossible d'accéder en écriture au fichier {KnxprojSourceFilePath}. "
-                              + "Veuillez vérifier que le programme a bien accès au fichier ou tentez de l'exécuter "
-                              + "en tant qu'administrateur.";
-                        Console.WriteLine(msg);
-                        writer.WriteLine(msg);
-                        KnxprojSourceFilePath = AskForPath();
-                        continue; // Retour au début de la boucle pour retenter l'extraction avec le nouveau path
-                    }
-                    catch (DirectoryNotFoundException)
-                    {
-                        // Si le dossier destination n'a pas été trouvé
-                        msg = $"[{DateTime.Now:dd/MM/yyyy - HH:mm:ss}] Le dossier {Path.GetDirectoryName(KnxprojSourceFilePath)} est introuvable. "
-                              + "Veuillez vérifier le chemin entré et réessayer.";
-                        Console.WriteLine(msg);
-                        writer.WriteLine(msg);
-                        KnxprojSourceFilePath = AskForPath();
-                        continue; // Retour au début de la boucle pour retenter l'extraction avec le nouveau path
+                        continue;
                     }
                     catch (PathTooLongException)
                     {
                         msg = $"[{DateTime.Now:dd/MM/yyyy - HH:mm:ss}] Erreur: le chemin {KnxprojSourceFilePath} est trop long (plus de 255 caractères). Veuillez réessayer.";
-                        Console.WriteLine(msg);
-                        writer.WriteLine(msg);
+                        App.ConsoleAndLogWriteLine(msg);
                         KnxprojSourceFilePath = AskForPath();
-                        continue; // Retour au début de la boucle pour retenter l'extraction avec le nouveau path
+                        continue;
                     }
 
-                    // Si le fichier a bien été transformé en zip, tentative d'extraction
+                    // Une fois que la normalisation de l'adresse du fichier du projet a été effectuée,
+                    // On tente de normaliser l'adresse du dossier projet exporté
                     try
                     {
-                        System.IO.Compression.ZipFile.ExtractToDirectory(zipArchivePath, KnxprojExportFolderPath); // On extrait le zip
+                        KnxprojExportFolderPath =
+                            Path.GetFullPath(KnxprojExportFolderPath); // Normalisation de l'adresse du dossier projet exporté
                     }
-                    catch (NotSupportedException)
+                    catch (ArgumentException)
                     {
-                        msg = $"[{DateTime.Now:dd/MM/yyyy - HH:mm:ss}] Erreur: Le type d'archive du fichier {Path.GetFileName(KnxprojSourceFilePath)} n'est pas supporté. "
-                              + "Veuillez vérifier que le fichier n'est pas corrompu. \nLe cas échéant, veuillez exporter à nouveau votre "
-                              + "projet ETS et réessayer de l'extraire.";
-                        Console.WriteLine(msg);
-                        writer.WriteLine(msg);
+                        msg = $"[{DateTime.Now:dd/MM/yyyy - HH:mm:ss}] Erreur: le chemin d'exportation du projet indiqué est vide. Veuillez réessayer.";
+                        App.ConsoleAndLogWriteLine(msg);
                         KnxprojExportFolderPath = AskForPath();
                         continue;
                     }
+                    catch (PathTooLongException)
+                    {
+                        msg = $"[{DateTime.Now:dd/MM/yyyy - HH:mm:ss}] Erreur: le chemin {KnxprojExportFolderPath} est trop long (plus de 255 caractères). Veuillez réessayer.";
+                        App.ConsoleAndLogWriteLine(msg);
+                        KnxprojExportFolderPath = AskForPath();
+                        continue;
+                    }
+
+                    managedToNormalizePaths = true;
+                }
+
+                /* ------------------------------------------------------------------------------------------------
+                ---------------------------------- EXTRACTION DU FICHIER KNXPROJ ----------------------------------
+                ------------------------------------------------------------------------------------------------ */
                     
-                    File.Delete(zipArchivePath); // On n'a plus besoin du zip, on le supprime
-                    msg = $"[{DateTime.Now:dd/MM/yyyy - HH:mm:ss}] Done ! New folder created: {KnxprojExportFolderPath}";
+                msg = $"[{DateTime.Now:dd/MM/yyyy - HH:mm:ss}] Starting to extract {Path.GetFileName(KnxprojSourceFilePath)}...";
+                App.ConsoleAndLogWriteLine(msg);
+
+                string zipArchivePath; // Adresse du fichier zip (utile pour la suite de manière à rendre le projet extractable)
+
+                // Transformation du knxproj en zip
+                if (KnxprojSourceFilePath.EndsWith(".knxproj"))
+                {
+                    // Si le fichier entré est un .knxproj
+                    zipArchivePath =
+                        KnxprojSourceFilePath.Substring(0, KnxprojSourceFilePath.Length - ".knxproj".Length) +
+                        ".zip"; // On enlève .knxproj et on ajoute .zip
+                }
+                else
+                {
+                    // Sinon, ce n'est pas le type de fichier que l'on veut
+                    msg = $"[{DateTime.Now:dd/MM/yyyy - HH:mm:ss}] Erreur: le fichier entré n'est pas au format .knxproj. "
+                          + "Veuillez réessayer. Pour obtenir un fichier dont l'extension est .knxproj, "
+                          + "rendez-vous dans votre tableau de bord ETS et cliquez sur \"Exporter le projet\"\n";
                     App.ConsoleAndLogWriteLine(msg);
-                    managedToExtractProject = true;
-                } 
+                    KnxprojSourceFilePath = AskForPath();
+                    continue; // Retour au début de la boucle pour retenter l'extraction avec le nouveau path
+                }
+
+                try
+                {
+                    // On essaie de transformer le fichier .knxproj en archive .zip
+                    File.Move(KnxprojSourceFilePath, zipArchivePath);
+                }
+                catch (FileNotFoundException)
+                {
+                    // Si le fichier n'existe pas ou que le path est incorrect
+                    msg = $"[{DateTime.Now:dd/MM/yyyy - HH:mm:ss}] Fichier {KnxprojSourceFilePath} introuvable. Veuillez vérifier le path que vous avez entré et réessayer.\n";
+                    App.ConsoleAndLogWriteLine(msg);
+                    KnxprojSourceFilePath = AskForPath();
+                    continue; // Retour au début de la boucle pour retenter l'extraction avec le nouveau path
+                }
+                catch (UnauthorizedAccessException)
+                {
+                    // Si le fichier n'est pas accessible en écriture
+                    msg = $"[{DateTime.Now:dd/MM/yyyy - HH:mm:ss}] Impossible d'accéder en écriture au fichier {KnxprojSourceFilePath}. "
+                          + "Veuillez vérifier que le programme a bien accès au fichier ou tentez de l'exécuter "
+                          + "en tant qu'administrateur.";
+                    App.ConsoleAndLogWriteLine(msg);
+                    KnxprojSourceFilePath = AskForPath();
+                    continue; // Retour au début de la boucle pour retenter l'extraction avec le nouveau path
+                }
+                catch (DirectoryNotFoundException)
+                {
+                    // Si le dossier destination n'a pas été trouvé
+                    msg = $"[{DateTime.Now:dd/MM/yyyy - HH:mm:ss}] Le dossier {Path.GetDirectoryName(KnxprojSourceFilePath)} est introuvable. "
+                          + "Veuillez vérifier le chemin entré et réessayer.";
+                    App.ConsoleAndLogWriteLine(msg);
+                    KnxprojSourceFilePath = AskForPath();
+                    continue; // Retour au début de la boucle pour retenter l'extraction avec le nouveau path
+                }
+                catch (PathTooLongException)
+                {
+                    msg = $"[{DateTime.Now:dd/MM/yyyy - HH:mm:ss}] Erreur: le chemin {KnxprojSourceFilePath} est trop long (plus de 255 caractères). Veuillez réessayer.";
+                    App.ConsoleAndLogWriteLine(msg);
+                    KnxprojSourceFilePath = AskForPath();
+                    continue; // Retour au début de la boucle pour retenter l'extraction avec le nouveau path
+                }
+
+                // Si le fichier a bien été transformé en zip, tentative d'extraction
+                try
+                {
+                    System.IO.Compression.ZipFile.ExtractToDirectory(zipArchivePath, KnxprojExportFolderPath); // On extrait le zip
+                }
+                catch (NotSupportedException)
+                {
+                    msg = $"[{DateTime.Now:dd/MM/yyyy - HH:mm:ss}] Erreur: Le type d'archive du fichier {Path.GetFileName(KnxprojSourceFilePath)} n'est pas supporté. "
+                          + "Veuillez vérifier que le fichier n'est pas corrompu. \nLe cas échéant, veuillez exporter à nouveau votre "
+                          + "projet ETS et réessayer de l'extraire.";
+                    App.ConsoleAndLogWriteLine(msg);
+                    KnxprojExportFolderPath = AskForPath();
+                    continue;
+                }
+                    
+                File.Delete(zipArchivePath); // On n'a plus besoin du zip, on le supprime
+                msg = $"[{DateTime.Now:dd/MM/yyyy - HH:mm:ss}] Done ! New folder created: {KnxprojExportFolderPath}";
+                App.ConsoleAndLogWriteLine(msg);
+                managedToExtractProject = true;
             }
         }
 
