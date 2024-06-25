5064712335d0bca488392533ea83cfa1d225ac4e
emmacstn <couston@insa-toulouse.fr>
2024-06-25 13:45:19 +0200

    .

--- KNXBoostDesktop/KNXBoostDesktop/obj/Debug/net8.0-windows/KNXBoostDesktop.AssemblyInfo.cs
+++ KNXBoostDesktop/KNXBoostDesktop/obj/Debug/net8.0-windows/KNXBoostDesktop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KNXBoostDesktop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4491a70ef6c741d10ddb5e85cfb02f1cd41392be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d8503a04055600aaf892a323c781f231314cd1d")]
 [assembly: System.Reflection.AssemblyProductAttribute("KNXBoostDesktop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KNXBoostDesktop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
