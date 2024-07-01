94d086943c8ac47e651b05ea37c1177909e435b9
emmacstn <couston@insa-toulouse.fr>
2024-07-01 11:52:07 +0200

    FormatterTranslate update

--- KNXBoostDesktop/KNXBoostDesktop/FormatterTranslate.cs
+++ KNXBoostDesktop/KNXBoostDesktop/FormatterTranslate.cs
@@ -1,42 +1,51 @@
-using System.Threading.Tasks;
+using System.Globalization;
+using System.Text.RegularExpressions;
 using DeepL;
 
 namespace KNXBoostDesktop;
 
 public class FormatterTranslate : Formatter
 {
-    public string sourceLanguage  = "fr";
-    public string destLanguage  = "en-Gb";
-
-
-
-
-    /*public override string Format(string input)
-    {
-
-
-        Task<string> task = GetTranslatedStringAsync(input);
-        task.Wait(); // Wait for the async task to complete
-
-        // Get the result after waiting
-        string result = task.Result;
-
-        return result;
-    }*/
+    private string _destLanguage = App.DisplayElements?.SettingsWindow.TranslationLang ?? string.Empty; 
     
     public override string Format(string input)
     {
-        return Task.Run(() => GetTranslatedStringAsync(input)).Result;
-    }
-    
-    
+        var translated = Task.Run(() => GetTranslatedStringAsync(input)).Result;
+        
+        // Remplacer toute la ponctuation et les signes spéciaux par des espaces
+        translated = Regex.Replace(translated, @"[\p{P}]", " ");
+        
+        // Mettre en minuscules
+        translated = translated.ToLower();
+        
+        // Remplacer les espaces par des underscores
+        translated = translated.Replace(" ", "_");
+        
+        // Séparer les mots et capitaliser chacun d'eux, puis les joindre sans underscore
+        string[] words = translated.Split(new[] { '_' }, StringSplitOptions.RemoveEmptyEntries);
 
-    public static async Task<string> GetTranslatedStringAsync(string input)
+        for (int i = 0; i < words.Length; i++)
+        {
+            words[i] = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(words[i]);
+        }
+
+        return string.Join("", words);
+        
+    }
+
+
+    private async Task<string> GetTranslatedStringAsync(string input)
     {
-        var authKey = "b2945c4a-90c9-40d6-ad6a-fa88854b7e42:fx";
-        var translator = new Translator(authKey);
-        return (await translator.TranslateTextAsync(input, "fr", "en-Gb")).Text;
+        var authKey = App.DisplayElements?.SettingsWindow.DecryptStringFromBytes(App.DisplayElements.SettingsWindow.DeeplKey);
+        switch (_destLanguage)
+        {
+            case "PT" : _destLanguage = "pt-Pt";
+                break;
+            case "EN" : _destLanguage = "en-Gb";
+                break;
+        }
+        var translator = new Translator(authKey ?? string.Empty); //call une exception ici
+        return (await translator.TranslateTextAsync(input, "fr", _destLanguage)).Text;
     }
 
 }
-
