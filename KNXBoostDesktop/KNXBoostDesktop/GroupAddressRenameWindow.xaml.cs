727da4354c39c7a462e3d301e0a146b0bbbf2daf
MaximeO <maxime.oliveira40@gmail.com>
2024-07-04 11:57:29 +0200

    Ajout de la fenêtre pour renommer les adresses de groupe

---
+++ KNXBoostDesktop/KNXBoostDesktop/GroupAddressRenameWindow.xaml.cs
@@ -0,0 +1,27 @@
+using System.Windows;
+using System.Windows.Input;
+
+namespace KNXBoostDesktop;
+
+public partial class GroupAddressRenameWindow : Window
+{
+    public GroupAddressRenameWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void Header_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void CancelButtonClick(object sender, RoutedEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void SaveButtonClick(object sender, RoutedEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+}
