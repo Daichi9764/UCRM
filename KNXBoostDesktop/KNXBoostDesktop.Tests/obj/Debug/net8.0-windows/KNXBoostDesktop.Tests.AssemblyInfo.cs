458471937512399900a47115d5f5c34dcae62452
MaximeO <maxime.oliveira40@gmail.com>
2024-07-08 11:20:53 +0200

    .

--- KNXBoostDesktop/KNXBoostDesktop.Tests/obj/Debug/net8.0-windows/KNXBoostDesktop.Tests.AssemblyInfo.cs
+++ KNXBoostDesktop/KNXBoostDesktop.Tests/obj/Debug/net8.0-windows/KNXBoostDesktop.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KNXBoostDesktop.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e06bb3069d86e12568e7fadde8d2b914802b0749")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff4139625fd4605d4e272bc62d2a18858623a07d")]
 [assembly: System.Reflection.AssemblyProductAttribute("KNXBoostDesktop.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KNXBoostDesktop.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
