2d7655d4c27ffb45d1056f3e6ef74650298577b7
MaximeO <maxime.oliveira40@gmail.com>
2024-07-10 10:27:14 +0200

    .

--- IncrementationVersion.sh
+++ IncrementationVersion.sh
@@ -3,24 +3,33 @@
 # Chemin vers le fichier app.xaml.cs
 filePath="./KNXBoostDesktop/KNXBoostDesktop/app.xaml.cs"
 
-# Lire tout le contenu du fichier
-fileContent=$(cat $filePath)
+# Lire tout le contenu du fichier ligne par ligne et stocker dans un tableau
+mapfile -t fileContent < "$filePath"
 
 # Initialiser une nouvelle variable pour stocker le contenu modifié
 newContent=""
 
 # Parcourir chaque ligne du fichier
-while IFS= read -r line
-do
-    if [[ $line =~ (public\ static\ readonly\ int\ AppBuild\ =\ )([0-9]+)(;) ]]; then
-        currentValue=${BASH_REMATCH[2]}
+for line in "${fileContent[@]}"; do
+    # Utiliser une regex pour capturer l'indentation, la valeur actuelle et le texte après le nombre
+    if [[ $line =~ ^(.*public\ static\ readonly\ int\ AppBuild\ =\ )([0-9]+)(;.*)$ ]]; then
+        # Extraire les parties de la ligne
+        indentation="${BASH_REMATCH[1]}"
+        currentValue="${BASH_REMATCH[2]}"
+        suffix="${BASH_REMATCH[3]}"
+        
+        # Incrémenter la valeur actuelle
         newValue=$((currentValue + 1))
-        line="${BASH_REMATCH[1]}${newValue}${BASH_REMATCH[3]}"
+        
+        # Construire la nouvelle ligne
+        line="${indentation}${newValue}${suffix}"
     fi
+    
+    # Ajouter la ligne modifiée ou non à la nouvelle variable de contenu
     newContent+="$line"$'\n'
-done <<< "$fileContent"
+done
 
 # Écrire le contenu modifié dans le fichier
-echo "$newContent" > $filePath
+echo "$newContent" > "$filePath"
 
 echo "AppBuild version incremented in $filePath"
