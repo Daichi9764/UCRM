a2fe90c975d13f8139069675ab1c0f5d1713a7df
MaximeO <maxime.oliveira40@gmail.com>
2024-07-10 10:48:18 +0200

    .

--- KNXBoostDesktop/KNXBoostDesktop/App.xaml.cs
+++ KNXBoostDesktop/KNXBoostDesktop/App.xaml.cs
@@ -38,7 +38,7 @@ namespace KNXBoostDesktop
         /// </summary>
         public static readonly float AppVersion = (float) 1.7; // Version de l'application
 
-        public static readonly int AppBuild = 304;
+        public static readonly int AppBuild = 306;
         
         
         // Gestion des logs
@@ -400,6 +400,8 @@ namespace KNXBoostDesktop
         }
     }
 }
+
+
 
 
 
