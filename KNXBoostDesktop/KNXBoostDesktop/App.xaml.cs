b2274f6d896add5f9b5bcb304d335389fae4abd5
MaximeO <maxime.oliveira40@gmail.com>
2024-07-15 10:59:25 +0200

    Build 324

--- KNXBoostDesktop/KNXBoostDesktop/App.xaml.cs
+++ KNXBoostDesktop/KNXBoostDesktop/App.xaml.cs
@@ -41,7 +41,7 @@ namespace KNXBoostDesktop
         /// </summary>
         public const float AppVersion = 1.8f; // Version de l'application
 
-        public static readonly int AppBuild = 323;
+        public static readonly int AppBuild = 324;
         
         
         // Gestion des logs
