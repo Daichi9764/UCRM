e1d1159f2ac834e1382f411f3142e05f3cfd6ac9
Hugo.M <151736047+MICHEL-Hugo@users.noreply.github.com>
2024-07-18 11:12:51 +0200

    Maj visibilité bouton Reload

--- KNXBoostDesktop/KNXBoostDesktop/MainWindow.xaml.cs
+++ KNXBoostDesktop/KNXBoostDesktop/MainWindow.xaml.cs
@@ -665,6 +665,9 @@ public partial class MainWindow
     /// <param name="e">The event data.</param>
     private async void ImportProjectButtonClick(object sender, RoutedEventArgs e)
     {
+        //Cacher le bouton de Reload
+        ButtonReload.Visibility = Visibility.Hidden;
+        
         App.ConsoleAndLogWriteLine("Waiting for user to select KNX project file");
         
         // Créer une nouvelle instance de OpenFileDialog
@@ -1284,7 +1287,7 @@ public partial class MainWindow
     /// <param name="e">The event data.</param>
     private void OpenParameters(object sender, RoutedEventArgs e)
     {
-
+        
         // Vérifie si la fenêtre de paramètres est déjà ouverte
         if (App.DisplayElements!.SettingsWindow != null && App.DisplayElements.SettingsWindow.IsVisible)
         {
