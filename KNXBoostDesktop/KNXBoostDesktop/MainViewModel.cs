cce718a5e7ba56da89e7c31591e9b70b245f0ea6
MaximeO <maxime.oliveira40@gmail.com>
2024-07-22 10:09:47 +0200

    Summary MainViewModel.cs

--- KNXBoostDesktop/KNXBoostDesktop/MainViewModel.cs
+++ KNXBoostDesktop/KNXBoostDesktop/MainViewModel.cs
@@ -2,10 +2,16 @@ using System.ComponentModel;
 
 namespace KNXBoostDesktop
 {
+    /// <summary>
+    /// ViewModel class that implements INotifyPropertyChanged to notify listeners of property changes.
+    /// </summary>
     public class MainViewModel : INotifyPropertyChanged
     {
         private bool _isProjectImported;
 
+        /// <summary>
+        /// Gets or sets a value indicating whether a project has been imported.
+        /// </summary>
         public bool IsProjectImported
         {
             get => _isProjectImported;
@@ -16,8 +22,15 @@ namespace KNXBoostDesktop
             }
         }
 
+        /// <summary>
+        /// Occurs when a property value changes.
+        /// </summary>
         public event PropertyChangedEventHandler? PropertyChanged;
 
+        /// <summary>
+        /// Raises the PropertyChanged event for a given property.
+        /// </summary>
+        /// <param name="propertyName">The name of the property that changed.</param>
         private void OnPropertyChanged(string propertyName)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
