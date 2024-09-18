4e2d373724959a617ee2d68b3eec409d627af5f3
MaximeO <maxime.oliveira40@gmail.com>
2024-09-18 16:03:11 +0200

    Edit config csproj

--- KNXBoostDesktop/KNXBoostDesktop/App.xaml.cs
+++ KNXBoostDesktop/KNXBoostDesktop/App.xaml.cs
@@ -45,7 +45,7 @@ public partial class App
     /// <summary>
     /// Represents the build of the application. Updated each time portions of code are merged on github.
     /// </summary>
-    public static readonly int AppBuild = 396;
+    public static readonly int AppBuild = 397;
         
         
     // Gestion des logs
@@ -686,6 +686,7 @@ public partial class App
         _writer?.Close();
     }
 }
+
 
 
 
